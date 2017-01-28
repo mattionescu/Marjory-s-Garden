583affd2f9d5af8bb0b74df722e83515ac000a8b
Felipe Linares <flinaresrm@gmail.com>
2017-01-28 17:52:40 -0500

    Add weather api

--- src/weather/WeatherFetcher.java
+++ src/weather/WeatherFetcher.java
@@ -3,6 +3,7 @@ package weather;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 
 /**
  * Write a description of class weather here
@@ -13,10 +14,11 @@ import com.google.api.client.json.JsonFactory;
 public class WeatherFetcher
 {
 
-    private static final String API_KEY = "";
+    private static final String API_KEY = "b78e47464fc1efb5ecd7bb06549343a5";
 
     static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-    static final JsonFactory JSON_FACTORY = new J
+    static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
     public WeatherFetcher()
     {
     }
